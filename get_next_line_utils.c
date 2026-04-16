967d3f787be7d5e0ede7eff11bd7847a116eb395
yoandria <yoandria@student.42antananarivo.mg>
2026-04-16 09:36:28 +0300

    Added files made from home to replace the content here

---
+++ get_next_line_utils.c
@@ -0,0 +1,75 @@
+#include "get_next_line.h"
+
+size_t	ft_strlen(char *s)
+{
+	size_t	i;
+
+	i = 0;
+	if (!s)
+		return (0);
+	while (s[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strchr(char *s, int c)
+{
+	int	i;
+
+	if (!s)
+		return (NULL);
+	if (c == 0)
+		return ((char *)&s[ft_strlen(s)]);
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] == (char)c)
+			return ((char *)&s[i]);
+		i++;
+	}
+	return (NULL);
+}
+
+static void	ft_fill_str(char *str, char *s1, char *s2)
+{
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	while (s1[i])
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (s2[j])
+		str[i++] = s2[j++];
+	str[i] = '\0';
+}
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	char	*str;
+
+	if (!s1)
+	{
+		s1 = malloc(sizeof(char));
+		if (!s1)
+			return (NULL);
+		s1[0] = '\0';
+	}
+	if (!s2)
+	{
+		free(s1);
+		return (NULL);
+	}
+	str = malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
+	if (!str)
+	{
+		free(s1);
+		return (NULL);
+	}
+	ft_fill_str(str, s1, s2);
+	free(s1);
+	return (str);
+}
